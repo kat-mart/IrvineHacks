c575505930ff61f222a5069eeb839ff2b6a51076
Kelly Ung <kellyung@myyahoo.com>
2025-01-25 10:07:28 -0800

    add navbar and routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Report from './components/Report';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hi</h1>
-      </header>
+    <div>
+        <Navbar />
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/Home' element={<Home />} />
+              <Route path='/Report' element={<Report />} />
+          </Routes>
     </div>
   );
 }
