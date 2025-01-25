4614f5cc987c075a3b2acc6d0f5f14203fdf9bcc
Kelly Ung <kellyung@myyahoo.com>
2025-01-25 10:08:15 -0800

    add components

---
+++ react-app/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <div class="header">
+                {/* <img src={logo} className="logo" alt="Logo" /> */}
+                <div className="site-title">Site Title</div>
+            </div>
+            <ul>
+                <li>
+                    <Link to='/Home'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/Report'>Report</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
